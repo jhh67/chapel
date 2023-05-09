e020b43c1a4a3db40340b1dd80815116c68ecfde
John H. Hartman <jhh67@users.noreply.github.com>
2023-05-09 10:14:10 -0700

    Revert lps_test.py, do this in a separate PR.
    Signed-off-by: John H. Hartman <jhh67@users.noreply.github.com>

--- test/runtime/jhh/lps_test.py
+++ test/runtime/jhh/lps_test.py
@@ -12,18 +12,9 @@ import os
 import sys
 from pprint import pprint
 
-if not 'CHPL_HOME' in os.environ:
-    print('CHPL_HOME is not set')
-    sys.exit(1)
-
-sys.path.append(os.path.join(os.environ['CHPL_HOME'], 'util', 'chplenv'))
-import printchplenv
-
-
 verbose = False
-skipReason = None
 
-def runCmd(cmd, env=None):
+def run(cmd, env=None):
     if type(cmd) is str:
         cmd = cmd.split()
     if env is None:
@@ -32,48 +23,33 @@ def runCmd(cmd, env=None):
     else:
         proc = subprocess.run(cmd, text=True, check=True, env=env,
             stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    return proc.stdout
+    return proc
 
-def skipif():
-    global skipReason
-
-    # Get the Chapel configuration
-    printchplenv.compute_all_values()
-    # strip the padding printchplenv puts on some of the keys
-    env = {k.strip():v for k,v in printchplenv.ENV_VALS.items()}
-
-    # Verify Chapel configuration
+def checkConfig():
     # These tests only run on comm=ofi and slurm-srun
-    if env.get('CHPL_LAUNCHER', None) != 'slurm-srun':
-        skipReason = "CHPL_LAUNCHER != slurm-srun"
-        return
-    if env.get('CHPL_COMM', None) != 'ofi':
-        skipReason = "CHPL_COMM != ofi"
-        return
+    proc = run("printchplenv --simple")
+    for line in proc.stdout.splitlines():
+        (key, value) = line.split('=')
+        if key == 'CHPL_COMM' and value != 'ofi':
+            raise unittest.SkipTest("CHPL_COMM != ofi")
+        if key == 'CHPL_LAUNCHER' and value != 'slurm-srun':
+            raise unittest.SkipTest("CHPL_LAUNCHER != slurm-srun")
 
 class LocalePerSocket(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         global verbose
-        cmd = ["sinfo", "--format=%X %Y %Z %i", "--noheader", "--exact"]
-        partition = os.environ.get('CHPL_LAUNCHER_PARTITION')
-        if partition is not None:
-            cmd += ["--partition", partition]
+        checkConfig()
+        cmd = ["sinfo", "--format=%X %Y %Z", "--noheader", "--exact"]
+        if 'CHPL_LAUNCHER_PARTITION' in os.environ:
+            cmd += ["--partition", os.environ['CHPL_LAUNCHER_PARTITION']]
             if verbose:
-                print("Partition: ", partition)
+                print("Partition: ", os.environ['CHPL_LAUNCHER_PARTITION'])
 
-        output = runCmd(cmd)
-        if output.startswith("Invalid node format specification: i"):
-            # sinfo doesn't support %i (reservation name)
-            cmd[1] = "--format=%X %Y %Z"
-            output = runCmd(cmd)
-        for line in output.splitlines():
-            fields = line.split()
-            # skip lines with reservation info
-            if len(fields) == 3:
-                (cls.sockets, cls.cores, cls.threads) = \
-                    [int(i) for i in fields]
-                break
+        proc = run(cmd)
+
+        (cls.sockets, cls.cores, cls.threads) = \
+            [int(i) for i in proc.stdout.splitlines()[0].split()]
 
         if verbose:
             print("Sockets: ", cls.sockets)
@@ -88,20 +64,14 @@ class LocalePerSocket(unittest.TestCase):
             del os.environ['CHPL_RT_LOCALES_PER_NODE']
 
         # Compile the test program
-        cmd = 'chpl lps_test.chpl'
         if verbose:
             print("Compiling test program")
-            print(cmd)
-        runCmd(cmd)
+        run('chpl lps_test.chpl')
         if verbose:
             print("Running tests")
 
     def setUp(self):
-        if skipReason is not None:
-            self.skipTest(skipReason)
-
-    def runCmd(self, cmd, env = None):
-        return runCmd(cmd, env)
+        pass
 
     def getCores(self, socket, reserved = 0):
         if socket == "all":
@@ -130,8 +100,8 @@ class LocalePerSocket(unittest.TestCase):
             self.assertNotIn('--cpu-bind', output)
 
     def mrAllocatedRequired(self):
-        output = runCmd("fi_info -v")
-        for line in output.splitlines():
+        proc = run("fi_info -v")
+        for line in proc.stdout.splitlines():
             if "mr_mode" in line:
                 if "FI_MR_ALLOCATED" in line:
                     return True
@@ -141,14 +111,14 @@ class LocalePerSocket(unittest.TestCase):
 
     def test_00_base(self):
         global cls
-        output = self.self.runCmd("./lps_test -nl 2 -v")
+        proc = run("./lps_test -nl 2 -v")
         self.checkArgs(2, 2, 1, self.sockets * self.cores * self.threads,
-                       None, output)
-        self.assertIn('oversubscribed = False', output)
+                       None, proc.stdout)
+        self.assertIn('oversubscribed = False', proc.stdout)
         self.assertIn('Using %d Shepherds' % (self.sockets * self.cores),
-                      output)
+                      proc.stdout)
         self.assertIn("QT_CPUBIND = " + self.getCores("all"),
-                      output)
+                      proc.stdout)
 
     def test_01_oversubscribed_env(self):
         # Being oversubscribed should have no effect on the number
@@ -157,47 +127,47 @@ class LocalePerSocket(unittest.TestCase):
             self.skipTest("FI_MR_ALLOCATED is required by provider")
         env = os.environ.copy()
         env['CHPL_RT_OVERSUBSCRIBED'] = 'true'
-        output = self.runCmd("./lps_test -nl 2 -v", env=env)
+        proc = run("./lps_test -nl 2 -v", env=env)
         self.checkArgs(2, 2, 1, self.sockets * self.cores * self.threads,
-                       None, output)
-        self.assertIn('oversubscribed = True', output)
+                       None, proc.stdout)
+        self.assertIn('oversubscribed = True', proc.stdout)
         self.assertIn('Using %s Shepherds' % (self.sockets * self.cores),
-                      output)
-        self.assertIn("QT_CPUBIND = " + self.getCores("all"), output)
+                      proc.stdout)
+        self.assertIn("QT_CPUBIND = " + self.getCores("all"), proc.stdout)
 
     def test_02_two_lpn(self):
         # One locale per socket. Each locale only uses the cores in
         # its socket.
         env = os.environ.copy()
         env['CHPL_RT_LOCALES_PER_NODE'] = '2'
-        output = self.runCmd("./lps_test -nl 2 -v", env=env)
-        self.checkArgs(1, 2, 2, self.sockets * self.cores, 'none', output)
-        self.assertIn('using socket 0', output)
-        self.assertIn('using socket 1', output)
-        self.assertIn('Using %s Shepherds' % self.cores, output)
-        self.assertIn("QT_CPUBIND = " + self.getCores(0), output)
-        self.assertIn("QT_CPUBIND = " + self.getCores(1), output)
+        proc = run("./lps_test -nl 2 -v", env=env)
+        self.checkArgs(1, 2, 2, self.sockets * self.cores, 'none', proc.stdout)
+        self.assertIn('using socket 0', proc.stdout)
+        self.assertIn('using socket 1', proc.stdout)
+        self.assertIn('Using %s Shepherds' % self.cores, proc.stdout)
+        self.assertIn("QT_CPUBIND = " + self.getCores(0), proc.stdout)
+        self.assertIn("QT_CPUBIND = " + self.getCores(1), proc.stdout)
 
     def test_03_four_lpn(self):
         # Four locales per node is an error if CHPL_RT_LOCALES_PER_NODE
         # is set.
         env = os.environ.copy()
         env['CHPL_RT_LOCALES_PER_NODE'] = '4'
-        output = self.runCmd(['./lps_test', '-nl', '4', '-v'], env=env)
+        proc = run(['./lps_test', '-nl', '4', '-v'], env=env)
         self.assertIn('error: The number of locales on the node is '
                       'greater than the number of sockets (4 > 2).',
-                      output)
+                      proc.stdout)
 
     def test_04_one_locale(self):
         # One locale uses only the cores in its socket.
         env = os.environ.copy()
         env['CHPL_RT_LOCALES_PER_NODE'] = '2'
-        output = self.runCmd(['./lps_test', '-nl', '1', '-v'], env=env)
+        proc = run(['./lps_test', '-nl', '1', '-v'], env=env)
         self.checkArgs(1, 1, 2, self.sockets * self.cores * self.threads,
-                       'none', output)
-        self.assertIn('using socket 0', output)
-        self.assertIn('Using %s Shepherds' % self.cores, output)
-        self.assertIn("QT_CPUBIND = " + self.getCores(0), output)
+                       'none', proc.stdout)
+        self.assertIn('using socket 0', proc.stdout)
+        self.assertIn('Using %s Shepherds' % self.cores, proc.stdout)
+        self.assertIn("QT_CPUBIND = " + self.getCores(0), proc.stdout)
 
     def test_05_two_lpn_oversubscribed(self):
         # CHPL_RT_OVERSUBSCRIBED should have no effect when there is
@@ -207,14 +177,14 @@ class LocalePerSocket(unittest.TestCase):
         env = os.environ.copy()
         env['CHPL_RT_LOCALES_PER_NODE'] = '2'
         env['CHPL_RT_OVERSUBSCRIBED'] = 'true'
-        output = self.runCmd("./lps_test -nl 2 -v", env=env)
-        self.checkArgs(1, 2, 2, self.sockets * self.cores, 'none', output)
-        self.assertIn('using socket 0', output)
-        self.assertIn('using socket 1', output)
-        self.assertIn('oversubscribed = True', output)
-        self.assertIn('Using %s Shepherds' % self.cores, output)
-        self.assertIn("QT_CPUBIND = " + self.getCores(0), output)
-        self.assertIn("QT_CPUBIND = " + self.getCores(1), output)
+        proc = run("./lps_test -nl 2 -v", env=env)
+        self.checkArgs(1, 2, 2, self.sockets * self.cores, 'none', proc.stdout)
+        self.assertIn('using socket 0', proc.stdout)
+        self.assertIn('using socket 1', proc.stdout)
+        self.assertIn('oversubscribed = True', proc.stdout)
+        self.assertIn('Using %s Shepherds' % self.cores, proc.stdout)
+        self.assertIn("QT_CPUBIND = " + self.getCores(0), proc.stdout)
+        self.assertIn("QT_CPUBIND = " + self.getCores(1), proc.stdout)
 
     def test_06_no_ht(self):
         # If SLURM_HINT=nomultithread then the launcher should request
@@ -222,27 +192,27 @@ class LocalePerSocket(unittest.TestCase):
         # same.
         env = os.environ.copy()
         env['SLURM_HINT'] = 'nomultithread'
-        output = self.runCmd("./lps_test -nl 2 -v", env=env)
-        self.checkArgs(2, 2, 1, self.sockets * self.cores, None, output)
-        self.assertNotIn('using socket', output)
-        self.assertNotIn('using socket', output)
+        proc = run("./lps_test -nl 2 -v", env=env)
+        self.checkArgs(2, 2, 1, self.sockets * self.cores, None, proc.stdout)
+        self.assertNotIn('using socket', proc.stdout)
+        self.assertNotIn('using socket', proc.stdout)
         self.assertIn('Using %s Shepherds' % (self.sockets * self.cores),
-                      output)
-        self.assertIn("QT_CPUBIND = " + self.getCores("all"), output)
+                      proc.stdout)
+        self.assertIn("QT_CPUBIND = " + self.getCores("all"), proc.stdout)
 
     def test_07_two_lpn_no_ht(self):
-        # If SLURM_HINT=nomultithread then each locale should only use one
-        # PU per core.
+        # If SLURM_HINT=nomultithread then each locale should only use one PU
+        # per core.
         env = os.environ.copy()
         env['CHPL_RT_LOCALES_PER_NODE'] = '2'
         env['SLURM_HINT'] = 'nomultithread'
-        output = self.runCmd("./lps_test -nl 2 -v", env=env)
-        self.checkArgs(1, 2, 2, self.sockets * self.cores, 'none', output)
-        self.assertIn('using socket 0', output)
-        self.assertIn('using socket 1', output)
-        self.assertIn('Using %s Shepherds' % self.cores, output)
-        self.assertIn("QT_CPUBIND = " + self.getCores(0), output)
-        self.assertIn("QT_CPUBIND = " + self.getCores(1), output)
+        proc = run("./lps_test -nl 2 -v", env=env)
+        self.checkArgs(1, 2, 2, self.sockets * self.cores, 'none', proc.stdout)
+        self.assertIn('using socket 0', proc.stdout)
+        self.assertIn('using socket 1', proc.stdout)
+        self.assertIn('Using %s Shepherds' % self.cores, proc.stdout)
+        self.assertIn("QT_CPUBIND = " + self.getCores(0), proc.stdout)
+        self.assertIn("QT_CPUBIND = " + self.getCores(1), proc.stdout)
 
     def test_08_ht_shepherds(self):
         # One worker per PU with hyperthreading.
@@ -250,14 +220,14 @@ class LocalePerSocket(unittest.TestCase):
         env['CHPL_RT_LOCALES_PER_NODE'] = '2'
         env['CHPL_RT_NUM_THREADS_PER_LOCALE'] = \
             '%s' % (self.sockets * self.cores)
-        output = self.runCmd("./lps_test -nl 2 -v", env=env)
-        self.checkArgs(1, 2, 2, self.sockets * self.cores, 'none', output)
-        self.assertIn('using socket 0', output)
-        self.assertIn('using socket 1', output)
+        proc = run("./lps_test -nl 2 -v", env=env)
+        self.checkArgs(1, 2, 2, self.sockets * self.cores, 'none', proc.stdout)
+        self.assertIn('using socket 0', proc.stdout)
+        self.assertIn('using socket 1', proc.stdout)
         self.assertIn('Using %s Shepherds' % (self.cores * self.threads),
-                      output)
-        self.assertIn("QT_CPUBIND = " + self.getThreads(0), output)
-        self.assertIn("QT_CPUBIND = " + self.getThreads(1), output)
+                      proc.stdout)
+        self.assertIn("QT_CPUBIND = " + self.getThreads(0), proc.stdout)
+        self.assertIn("QT_CPUBIND = " + self.getThreads(1), proc.stdout)
 
     def test_09_no_ht_ht_shepherds(self):
         # When SLURM_HINT=nomultithread and
@@ -268,26 +238,26 @@ class LocalePerSocket(unittest.TestCase):
         env['CHPL_RT_LOCALES_PER_NODE'] = '2'
         env['CHPL_RT_NUM_THREADS_PER_LOCALE'] = str(self.sockets * self.cores)
         env['SLURM_HINT'] = 'nomultithread'
-        output = self.runCmd("./lps_test -nl 2 -v", env=env)
-        self.checkArgs(1, 2, 2, self.cores * self.threads, 'none', output)
-        self.assertIn('using socket 0', output)
-        self.assertIn('using socket 1', output)
-        self.assertIn('Using %s Shepherds' % self.cores, output)
-        self.assertIn("QT_CPUBIND = " + self.getCores(0), output)
-        self.assertIn("QT_CPUBIND = " + self.getCores(1), output)
+        proc = run("./lps_test -nl 2 -v", env=env)
+        self.checkArgs(1, 2, 2, self.cores * self.threads, 'none', proc.stdout)
+        self.assertIn('using socket 0', proc.stdout)
+        self.assertIn('using socket 1', proc.stdout)
+        self.assertIn('Using %s Shepherds' % self.cores, proc.stdout)
+        self.assertIn("QT_CPUBIND = " + self.getCores(0), proc.stdout)
+        self.assertIn("QT_CPUBIND = " + self.getCores(1), proc.stdout)
 
     def test_10_reserved(self):
         # One core is reserved for the AM handler thread
         env = os.environ.copy()
         env['CHPL_RT_LOCALES_PER_NODE'] = '2'
         env['CHPL_RT_COMM_OFI_DEDICATED_AMH_CORES'] = 'true'
-        output = self.runCmd("./lps_test -nl 2 -v", env=env)
-        self.checkArgs(1, 2, 2, self.sockets * self.cores, 'none', output)
-        self.assertIn('using socket 0', output)
-        self.assertIn('using socket 1', output)
-        self.assertIn('Using %s Shepherds' % (self.cores - 1), output)
-        self.assertIn("QT_CPUBIND = " + self.getCores(0,1), output)
-        self.assertIn("QT_CPUBIND = " + self.getCores(1,1), output)
+        proc = run("./lps_test -nl 2 -v", env=env)
+        self.checkArgs(1, 2, 2, self.sockets * self.cores, 'none', proc.stdout)
+        self.assertIn('using socket 0', proc.stdout)
+        self.assertIn('using socket 1', proc.stdout)
+        self.assertIn('Using %s Shepherds' % (self.cores - 1), proc.stdout)
+        self.assertIn("QT_CPUBIND = " + self.getCores(0,1), proc.stdout)
+        self.assertIn("QT_CPUBIND = " + self.getCores(1,1), proc.stdout)
 
     def test_11_ht_reserved(self):
         # One core is reserved for the AM handler thread when using
@@ -296,16 +266,16 @@ class LocalePerSocket(unittest.TestCase):
         env['CHPL_RT_LOCALES_PER_NODE'] = '2'
         env['CHPL_RT_COMM_OFI_DEDICATED_AMH_CORES'] = 'true'
         env['CHPL_RT_NUM_THREADS_PER_LOCALE'] = str(self.sockets * self.cores)
-        output = self.runCmd("./lps_test -nl 2 -v", env=env)
-        self.checkArgs(1, 2, 2, self.sockets * self.cores, 'none', output)
-        self.assertIn('using socket 0', output)
-        self.assertIn('using socket 1', output)
+        proc = run("./lps_test -nl 2 -v", env=env)
+        self.checkArgs(1, 2, 2, self.sockets * self.cores, 'none', proc.stdout)
+        self.assertIn('using socket 0', proc.stdout)
+        self.assertIn('using socket 1', proc.stdout)
         self.assertIn('Using %d Shepherds' % (self.sockets * self.cores - 2),
-                      output)
+                      proc.stdout)
         self.assertIn("QT_CPUBIND = " + self.getThreads(0,1),
-                      output)
+                      proc.stdout)
         self.assertIn("QT_CPUBIND = " + self.getThreads(1,1),
-                      output)
+                      proc.stdout)
 
     @unittest.skip("should not be considered oversubscribed")
     def test_12_oversubscribed_no_env(self):
@@ -314,13 +284,12 @@ class LocalePerSocket(unittest.TestCase):
         env = os.environ.copy()
         env['PMI_MAX_KVS_ENTRIES'] = '20'
         env['PMI_NO_PREINITIALIZE'] = 'y'
-        output = self.runCmd(['srun', '-l', '--quiet', '--nodes=1',
-                    '--ntasks=3',
+        proc = run(['srun', '-l', '--quiet', '--nodes=1', '--ntasks=3',
                     '--ntasks-per-node=3', '--cpus-per-task=16',
                     '--exclusive', '--kill-on-bad-exit',
                     './lps_test_real', '-nl', '3', '-v'], env=env)
-        self.assertIn('oversubscribed = True', output)
-        self.assertIn('Using 8 Shepherds', output)
+        self.assertIn('oversubscribed = True', proc.stdout)
+        self.assertIn('Using 8 Shepherds', proc.stdout)
 
 def main(argv):
     global verbose
@@ -330,22 +299,7 @@ def main(argv):
         argv.remove("-f")
     if "-v" in argv:
         verbose = True
-
-    skipif()
-
-    prog = unittest.main(argv=argv, failfast=failfast)
-
-    # Produce output that start_test can parse. To start_test this is a single
-    # test. Report report success if all tests succeeded, an error if any
-    # test failed, and nothing if all tests were skipped.
-
-    if len(prog.result.skipped) > 0:
-        print("Skipped %d tests" % len(prog.result.skipped))
-    if len(prog.result.skipped) != prog.result.testsRun:
-        if len(prog.result.errors) > 0 or len(prog.result.failures) > 0:
-            print("[Error running tests]")
-        else:
-            print("[Success matching test results]")
+    unittest.main(argv=argv, failfast=failfast)
 
 if __name__ == '__main__':
     main(sys.argv)
