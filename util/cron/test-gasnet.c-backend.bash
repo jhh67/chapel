524fd433d9c4ffb67d7e4f2da6494e7dec221faf
John H. Hartman <jhh67@users.noreply.github.com>
2024-01-11 15:23:13 -0700

    WIP

--- util/cron/test-gasnet.c-backend.bash
+++ util/cron/test-gasnet.c-backend.bash
@@ -14,6 +14,7 @@ export CHPL_NIGHTLY_TEST_CONFIG_NAME="gasnet.c-backend"
 unset CHPL_NIGHTLY_TEST_DIRS
 
 export CHPL_NIGHTLY_TEST_DIRS=runtime/jhh/colocales
+echo XXX
 which make
 
 $CWD/nightly -cron -multilocale ${nightly_args}
