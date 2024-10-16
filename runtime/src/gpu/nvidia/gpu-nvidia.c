073c926d1d1dfc191c295c2140514ad618abd2ae
John H. Hartman <jhh67@users.noreply.github.com>
2024-10-16 14:39:53 -0700

    debug message

--- runtime/src/gpu/nvidia/gpu-nvidia.c
+++ runtime/src/gpu/nvidia/gpu-nvidia.c
@@ -358,8 +358,9 @@ void chpl_gpu_impl_mem_free(void* memAlloc) {
     CUDA_CALL(cuPointerGetAttribute((void*)&dev_id,
                                     CU_POINTER_ATTRIBUTE_DEVICE_ORDINAL,
                                     (CUdeviceptr)memAlloc));
-    fprintf(stderr, "chpl_gpu_impl_mem_free %d\n", dev_id);
-    switch_context(dev_id);
+    int index = deviceIDToIndex[dev_id];
+    fprintf(stderr, "chpl_gpu_impl_mem_free dev_id %d index %d\n", dev_id);
+    switch_context(index];
 
 #ifdef CHPL_GPU_MEM_STRATEGY_ARRAY_ON_DEVICE
     if (chpl_gpu_impl_is_host_ptr(memAlloc)) {
