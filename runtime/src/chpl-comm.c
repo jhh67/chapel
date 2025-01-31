8f3ef2b7a4ea69ba45a9ec8ff5dababbca563000
John H. Hartman <jhh67@users.noreply.github.com>
2025-01-31 10:51:00 -0700

    WIP

--- runtime/src/chpl-comm.c
+++ runtime/src/chpl-comm.c
@@ -186,9 +186,9 @@ static void *touch_thread(void *mem_region) {
   uintptr_t aligned_offset = (uintptr_t)aligned_start - (uintptr_t)mr->start;
   uintptr_t aligned_size = round_down_to_mask(mr->size - aligned_offset, touch_size-1);
 
-  fprintf(stderr, "tid %d page_size %lu touch_size %lu start %p size %lu\n",
-          mr->tid, page_size, touch_size, mr->start, mr->size);
-  fprintf(stderr, "aligned_start %p aligned_offset %lu aligned_size %lu\n",
+  fprintf(stderr, "%d: tid %d page_size %lu touch_size %lu start %p size %lu "
+                  "aligned_start %p aligned_offset %lu aligned_size %lu\n",
+          chpl_nodeID, mr->tid, page_size, touch_size, mr->start, mr->size,
           aligned_start, aligned_offset, aligned_size);
   chpl_topo_setThreadLocality(mr->tid % chpl_topo_getNumNumaDomains());
   // Iterate through all the touch regions cyclically
