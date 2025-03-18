2d645cff3ecef52a13da2d9d11b766fd267e9233
John H. Hartman <jhh67@users.noreply.github.com>
2025-03-18 14:08:38 -0700

    Added missing util/chplenv/chpl_flavor.py
    Signed-off-by: John H. Hartman <jhh67@users.noreply.github.com>

---
+++ util/chplenv/chpl_flavor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import sys
+
+import overrides
+from utils import memoize
+
+
+@memoize
+def get():
+    return overrides.get('CHPL_FLAVOR')
+
+def _main():
+    flavor_val = get()
+    sys.stdout.write("{0}\n".format(flavor_val))
+
+
+if __name__ == '__main__':
+    _main()
